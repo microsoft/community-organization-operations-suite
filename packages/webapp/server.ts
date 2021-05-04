6e149206a8a9d1717cd6f3df3f978f189f913f91
Chris Trevino <chtrevin@microsoft.com>
2021-05-04 14:51:19 -0700

    update server configs

--- packages/webapp/server.ts
+++ packages/webapp/server.ts
@@ -2,16 +2,39 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import config from 'config'
+import type { IConfig } from 'config'
+import { config as configDotEnv } from 'dotenv'
 import express, { Express } from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import next from 'next'
 
-async function bootstrap(): Promise<void> {
-	const port = config.get<number>('server.port')
-	const dev = config.get<boolean>('server.devMode')
-	const apiUrl = config.get<string>('api.url')
-	console.log(`starting webapp; devmode? ${dev}; apiUrl=${apiUrl}; port=${port}`)
+class Configuration {
+	public constructor(private config: IConfig) {}
+
+	public get isDevMode(): boolean {
+		return this.config.get<boolean>('server.devMode')
+	}
+
+	public get port(): number {
+		return this.config.get<number>('server.port')
+	}
+
+	public get apiUrl(): string {
+		return this.config.get<string>('api.url')
+	}
+}
+
+function createConfig(): Configuration {
+	// load dotenv first before initializing config stack
+	configDotEnv()
+	// eslint-disable-next-line
+	return new Configuration(require('config') as IConfig)
+}
+
+async function createServer(config: Configuration): Promise<Express> {
+	const dev = config.isDevMode
+	const apiUrl = config.apiUrl
+	console.log(`launching webapp; devmode? ${dev}; apiUrl=${apiUrl}`)
 
 	const app = next({ dev })
 	const handle = app.getRequestHandler()
@@ -37,6 +60,13 @@ async function bootstrap(): Promise<void> {
 		}) as any
 	)
 	server.all('*', (req, res) => handle(req, res))
+	return server
+}
+
+async function bootstrap() {
+	const config = createConfig()
+	const port = config.port
+	const server = await createServer(config)
 	server.listen(port, () => {
 		console.log(`🚀 greenlight app ready on http://localhost:${port}`)
 	})
