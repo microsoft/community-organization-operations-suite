9fb4f6b0e719167f0a53bd394026b056e9a9377c
Chris Trevino <chtrevin@microsoft.com>
2021-05-06 10:37:42 -0700

    correct import

--- packages/webapp/types/Auth.ts
+++ packages/webapp/types/Auth.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import type User from '~types/user'
+import type User from '~types/User'
 
 export default interface Auth {
 	user?: User
