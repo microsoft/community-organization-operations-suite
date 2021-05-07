9e200f4fbd0e429f30755ec01040a4dc41b32fca
Devan Huapaya <devan@genui.com>
2021-05-07 13:58:53 -0700

    Added fluent chevron to details list title

--- packages/webapp/components/ui/DetailsListTitle/index.tsx
+++ packages/webapp/components/ui/DetailsListTitle/index.tsx
@@ -2,10 +2,11 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-// import styles from './index.module.scss'
+import { FontIcon } from '@fluentui/react'
+import cx from 'classnames'
+import styles from './index.module.scss'
 import type ComponentProps from '~types/ComponentProps'
 // TODO: add right chevron element
-// import { FontIcon } from '@fluentui/react'
 
 interface DetailsListTitleProps extends ComponentProps {
 	title?: string
@@ -13,9 +14,9 @@ interface DetailsListTitleProps extends ComponentProps {
 
 export default function DetailsListTitle({ children }: DetailsListTitleProps): JSX.Element {
 	return (
-		<h2 className='d-flex align-items-center'>
+		<h2 className={cx('d-flex align-items-center', styles.detailsListTitle)}>
 			{/* TODO: add right chevron element */}
-			{/* <FontIcon className='me-2' iconName='ChevronRightSmall' /> */}
+			<FontIcon className={cx('me-2', styles.icon)} iconName='ChevronRight' />
 			{/* TODO: consider adding transform on hover */}
 			{children}
 		</h2>
