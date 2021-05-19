6d08767452f30ce691b5c0ef85f374913b833397
Batch Mosqueda <batch@genui.com>
2021-05-19 10:48:14 -0700

    added some validations

--- packages/webapp/src/components/forms/NewNavigatorActionForm/index.tsx
+++ packages/webapp/src/components/forms/NewNavigatorActionForm/index.tsx
@@ -2,11 +2,13 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { DatePicker, FontIcon, IDatePicker, PrimaryButton } from '@fluentui/react'
+import { DatePicker, FontIcon, IDatePicker, PrimaryButton, addYears } from '@fluentui/react'
+import { useConst } from '@fluentui/react-hooks'
 import cx from 'classnames'
 import { Formik, Form, Field } from 'formik'
 import { useRef, useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
+import * as yup from 'yup'
 import styles from './index.module.scss'
 import type ComponentProps from '~types/ComponentProps'
 
@@ -14,11 +16,20 @@ interface NewNavigatorActionFormProps extends ComponentProps {
 	title?: string
 }
 
+const NewNavigatorValidationSchema = yup.object().shape({
+	firstName: yup.string().min(2, 'Too Short!').max(25, 'Too Long!').required('Required'),
+	lastName: yup.string().min(2, 'Too Short!').max(25, 'Too Long!').required('Required'),
+	email: yup.string().email('Invalid email'),
+	phone: yup.number().typeError('Must be numeric'),
+	zipCode: yup.number().typeError('Must be numeric')
+})
+
 export default function NewNavigatorActionForm({
 	className
 }: NewNavigatorActionFormProps): JSX.Element {
 	const [birthdate, setBirthdate] = useState<Date | undefined>()
 	const datePickerRef = useRef<IDatePicker>(null)
+	const maxDate = useConst(addYears(new Date(Date.now()), -18))
 
 	return (
 		<div className={cx(className)}>
@@ -29,21 +40,21 @@ export default function NewNavigatorActionForm({
 					lastName: '',
 					birthdate: '',
 					email: '',
-					phone: '',
+					phone: null,
 					address: '',
 					unit: '',
 					city: '',
 					state: '',
-					zipCode: '',
+					zipCode: null,
 					identifiers: ''
 				}}
+				validationSchema={NewNavigatorValidationSchema}
 				onSubmit={values => {
 					values.birthdate = new Intl.DateTimeFormat('en-US').format(birthdate)
 					console.log('Form Submit', values)
 				}}
 			>
-				{({ values, errors, touched }) => {
-					const hasError = errors.firstName && touched.firstName
+				{({ values, errors }) => {
 					return (
 						<>
 							<h3 className={cx(styles.header)}>
@@ -69,9 +80,16 @@ export default function NewNavigatorActionForm({
 												placeholder='Firstname'
 												className={cx(styles.field)}
 											/>
+											{/* Handle errors */}
+											{errors.firstName ? (
+												<div className='p-2 px-3 text-danger'>{errors.firstName}</div>
+											) : null}
 										</Col>
 										<Col>
 											<Field name='lastName' placeholder='Lastname' className={cx(styles.field)} />
+											{errors.lastName ? (
+												<div className='p-2 px-3 text-danger'>{errors.lastName}</div>
+											) : null}
 										</Col>
 										<Col>
 											<DatePicker
@@ -80,6 +98,8 @@ export default function NewNavigatorActionForm({
 												allowTextInput
 												ariaLabel='Select a date'
 												value={birthdate}
+												maxDate={maxDate}
+												initialPickerDate={maxDate}
 												onSelectDate={setBirthdate as (date: Date | null | undefined) => void}
 												styles={{
 													root: {
@@ -118,7 +138,13 @@ export default function NewNavigatorActionForm({
 												placeholder='Email address'
 												className={cx(styles.field)}
 											/>
+											{errors.email ? (
+												<div className='p-2 px-3 text-danger'>{errors.email}</div>
+											) : null}
 											<Field name='phone' placeholder='Phone #' className={cx(styles.field)} />
+											{errors.phone ? (
+												<div className='p-2 px-3 text-danger'>{errors.phone}</div>
+											) : null}
 										</Col>
 									</Row>
 								</section>
@@ -145,6 +171,9 @@ export default function NewNavigatorActionForm({
 										</Col>
 										<Col>
 											<Field name='zipCode' placeholder='Zip Code' className={cx(styles.field)} />
+											{errors.zipCode ? (
+												<div className='p-2 px-3 text-danger'>{errors.zipCode}</div>
+											) : null}
 										</Col>
 									</Row>
 								</section>
@@ -174,8 +203,6 @@ export default function NewNavigatorActionForm({
 									</Row>
 								</section>
 							</Form>
-							{/* Handle errors */}
-							{hasError ? <div className='p-2 px-3 text-danger'>{errors.firstName}</div> : null}
 						</>
 					)
 				}}
