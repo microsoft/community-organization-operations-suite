678c2156e4d0cd6661bfc848dbea94927dc860f7
Batch Mosqueda <batch@genui.com>
2021-06-09 12:56:19 -0700

    updated rendering for mobile

--- packages/webapp/src/components/lists/SpecialistList/index.tsx
+++ packages/webapp/src/components/lists/SpecialistList/index.tsx
@@ -53,12 +53,11 @@ export default function SpecialistList({
 	const [filteredList, setFilteredList] = useState<User[]>(sortedList)
 
 	const openSpecialistDetails = useCallback(
-		(sid: string) => {
-			const selectedSpecialist = specialistList.find((s: User) => s.id === sid)
+		(selectedSpecialist: User) => {
 			setSpecialist(selectedSpecialist)
 			openSpecialistPanel()
 		},
-		[openSpecialistPanel, specialistList]
+		[openSpecialistPanel]
 	)
 
 	const searchList = useCallback(
@@ -99,7 +98,7 @@ export default function SpecialistList({
 						tag='span'
 						title={`${user.name.first} ${user.name.last}`}
 						titleLink='/'
-						onClick={() => openSpecialistDetails(user.id)}
+						onClick={() => openSpecialistDetails(user)}
 					/>
 				)
 			}
@@ -127,8 +126,8 @@ export default function SpecialistList({
 			key: 'actionColumn',
 			name: '',
 			className: 'w-100 d-flex justify-content-end',
-			onRenderColumnItem: function onRenderColumnItem(item: User) {
-				return <MultiActionButton columnItem={item} buttonGroup={columnActionButtons} />
+			onRenderColumnItem: function onRenderColumnItem(user: User) {
+				return <MultiActionButton columnItem={user} buttonGroup={columnActionButtons} />
 			}
 		}
 	]
@@ -149,16 +148,16 @@ export default function SpecialistList({
 								<Row className='ps-2 pb-4'>{user?.roles?.map(r => r.roleType).join(', ')}</Row>
 								<Row className='ps-2'>
 									<Col>
-										<Row># of Engagements</Row>
-										<Row>0</Row>
+										<Row># of Assigned Engagements</Row>
+										<Row>{user.activeEngagementCount}</Row>
 									</Col>
 									<Col className={cx('d-flex justify-content-end')}>
-										<MultiActionButton buttonGroup={columnActionButtons} />
+										<MultiActionButton columnItem={user} buttonGroup={columnActionButtons} />
 									</Col>
 								</Row>
 							</Col>
 						}
-						onClick={() => openSpecialistDetails(user.id)}
+						onClick={() => openSpecialistDetails(user)}
 					/>
 				)
 			}
