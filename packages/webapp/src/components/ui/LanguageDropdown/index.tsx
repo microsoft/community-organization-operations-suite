272d3b634085bbb2899b6c532f6645c0ed87f116
Batch Mosqueda <v-bmosqueda@microsoft.com>
2021-06-29 18:14:12 -0700

    added language dropdown

---
+++ packages/webapp/src/components/ui/LanguageDropdown/index.tsx
@@ -0,0 +1,99 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import { memo } from 'react'
+import type ComponentProps from '~types/ComponentProps'
+import { Dropdown } from '@fluentui/react'
+import ClientOnly from '~ui/ClientOnly'
+import cx from 'classnames'
+
+interface LanguageDropdownProps extends ComponentProps {
+	locale: string
+	locales: string[]
+	onChange?: (locale: string) => void
+}
+
+const LanguageDropdown = memo(function LanguageDropdown({
+	className,
+	locale,
+	locales,
+	onChange
+}: LanguageDropdownProps): JSX.Element {
+	const localeOptions = locales.map(loc => {
+		// @ts-expect-error DisplayNames not on Intl
+		const languageName = new Intl.DisplayNames([locale], {
+			type: 'language'
+		})
+		return {
+			key: loc,
+			text: languageName.of(loc)
+		}
+	})
+
+	return (
+		<ClientOnly>
+			<Dropdown
+				id='languageDropdown'
+				options={localeOptions}
+				defaultSelectedKey={locale}
+				onChange={(_ev, option) => onChange?.(option.key as string)}
+				className={cx(className)}
+				styles={{
+					root: {
+						marginRight: 20
+					},
+					dropdown: {
+						fontSize: 12,
+						border: 'none',
+						selectors: {
+							':focus': {
+								'.ms-Dropdown-title': {
+									color: 'white'
+								},
+								'.ms-Dropdown-caretDown': {
+									color: 'white'
+								}
+							},
+							':hover': {
+								'.ms-Dropdown-title': {
+									color: 'white'
+								},
+								'.ms-Dropdown-caretDown': {
+									color: 'white'
+								}
+							},
+							':active': {
+								'.ms-Dropdown-title': {
+									color: 'white'
+								},
+								'.ms-Dropdown-caretDown': {
+									color: 'white'
+								}
+							}
+						}
+					},
+					title: {
+						backgroundColor: 'transparent',
+						color: 'white',
+						border: 'none',
+						outline: 'none'
+					},
+					caretDown: {
+						color: 'white'
+					},
+					dropdownItem: {
+						fontSize: 12
+					},
+					dropdownItemSelected: {
+						fontSize: 12
+					},
+					dropdownItemSelectedAndDisabled: {
+						fontSize: 12
+					}
+				}}
+			/>
+		</ClientOnly>
+	)
+})
+export default LanguageDropdown
