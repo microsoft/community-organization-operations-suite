563be9d01b1872d072e33e087da80855a0d1d5f4
Batch Mosqueda <batch@genui.com>
2021-07-01 17:25:22 -0700

    account localizations

--- packages/webapp/src/pages/account.tsx
+++ packages/webapp/src/pages/account.tsx
@@ -10,23 +10,25 @@ import ProfileForm from '~forms/ProfileForm'
 import { memo } from 'react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { GetStaticProps } from 'next'
+import { useTranslation } from 'next-i18next'
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
 	return {
 		props: {
-			...(await serverSideTranslations(locale, ['common', 'footer']))
+			...(await serverSideTranslations(locale, ['common', 'footer', 'account']))
 		}
 	}
 }
 
 const AccountPage = memo(function AccountPage(): JSX.Element {
+	const { t } = useTranslation('account')
 	const { authUser } = useAuthUser()
 	const userRole = get(authUser, 'user.roles[0]')
 	const { data: orgData } = useOrganization(userRole?.orgId)
 	const user = orgData?.users?.find(u => u.id === authUser?.user?.id)
 
 	return (
-		<ContainerLayout orgName={orgData?.name} documentTitle='My Profile'>
+		<ContainerLayout orgName={orgData?.name} documentTitle={t('page.title')}>
 			<ProfileForm user={user} />
 		</ContainerLayout>
 	)
