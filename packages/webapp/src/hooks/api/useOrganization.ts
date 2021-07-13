5b73b9f45cc63a4b5b824c815765fec7ee3bd81a
Devan Huapaya <devan@genui.com>
2021-07-13 09:57:20 -0700

    Lint fix

--- packages/webapp/src/hooks/api/useOrganization.ts
+++ packages/webapp/src/hooks/api/useOrganization.ts
@@ -4,7 +4,7 @@
  */
 import { gql, useLazyQuery } from '@apollo/client'
 import { ApiResponse } from './types'
-import type { Organization, OrganizationIdInput } from '@greenlight/schema/lib/client-types'
+import type { Organization } from '@greenlight/schema/lib/client-types'
 import { OrgFields } from '~hooks/api/fragments'
 import { organizationState } from '~store'
 import { useRecoilState } from 'recoil'
@@ -23,35 +23,51 @@ export const GET_ORGANIZATION = gql`
 
 export interface UseOranizationReturn extends ApiResponse<Organization> {
 	organization?: Organization
+	loadOrganization: (id: string) => void
 }
 
 export function useOrganization(orgId?: string): UseOranizationReturn {
+	// Common translations
 	const { c } = useTranslation()
-	const [load, { loading, error, data }] = useLazyQuery(GET_ORGANIZATION, {
+
+	/**
+	 * Lazy graphql query.
+	 * @params
+	 * @returns an array with the first element being the load function and the second element being the graphql returns
+	 *
+	 *
+	 * */
+	const [load, { loading, error }] = useLazyQuery(GET_ORGANIZATION, {
 		fetchPolicy: 'cache-and-network',
 		onCompleted: data => {
 			if (data?.organization) {
 				setOrg(data.organization)
 			}
-		}
-	})
-	const [organization, setOrg] = useRecoilState<Organization | null>(organizationState)
-
-	useEffect(() => {
-		if (error) {
+		},
+		onError: error => {
 			console.error(c('hooks.useOrganization.loadData.failed'), error)
 		}
-	}, [error, c])
+	})
 
+	// Recoil state used to store and return the cached organization
+	const [organization, setOrg] = useRecoilState<Organization | null>(organizationState)
+
+	// If an orgId was passed execute the load function immediately
+	// Otherwise, just return the organization and do NOT make a graph query
 	useEffect(() => {
 		if (orgId) {
 			load({ variables: { body: { orgId } } })
 		}
 	}, [orgId, load])
 
+	const loadOrganization: UseOranizationReturn['loadOrganization'] = id => {
+		load({ variables: { body: { orgId: id } } })
+	}
+
 	return {
 		loading,
 		error,
+		loadOrganization,
 		organization
 	}
 }
