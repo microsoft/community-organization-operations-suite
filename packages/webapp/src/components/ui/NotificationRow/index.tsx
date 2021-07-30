3ead88453b890b766e2021716e53d44ca58e6dc3
Batch Mosqueda <batch@genui.com>
2021-07-30 16:30:37 -0700

    applied localization to notification row item content

--- packages/webapp/src/components/ui/NotificationRow/index.tsx
+++ packages/webapp/src/components/ui/NotificationRow/index.tsx
@@ -7,7 +7,7 @@ import cx from 'classnames'
 import type ComponentProps from '~types/ComponentProps'
 import type { Mention } from '@resolve/schema/lib/client-types'
 import formatTimeFromToday from '~utils/formatTimeFromToday'
-import { memo } from 'react'
+import { memo, Fragment } from 'react'
 import { useTranslation } from '~hooks/useTranslation'
 import Icon from '~ui/Icon'
 import ShortString from '~ui/ShortString'
@@ -25,6 +25,50 @@ const NotificationRow = memo(function NotificationRow({
 }: NotificationRowProps): JSX.Element {
 	const { c } = useTranslation()
 
+	const getNotificationItemBody = (): (string | JSX.Element)[] => {
+		const content = []
+		const words = c('notification.row.text').split(' ')
+		let isLastWordaPlaceholder = false
+		for (let i = 0; i < words.length; i++) {
+			if (words[i] === '[[sender]]') {
+				content.push(
+					<Fragment key={i}>
+						<span className='text-primary'>
+							{mention?.createdBy?.name.first} {mention?.createdBy?.name.last}
+						</span>{' '}
+					</Fragment>
+				)
+				isLastWordaPlaceholder = true
+				continue
+			}
+
+			if (words[i] === '[[description]]') {
+				content.push(
+					<Fragment key={i}>
+						<span className={styles.description}>
+							<ShortString
+								text={mention?.engagement?.description}
+								limit={30}
+								showReadMoreLess={false}
+							/>
+						</span>{' '}
+					</Fragment>
+				)
+				isLastWordaPlaceholder = true
+				continue
+			}
+
+			if (isLastWordaPlaceholder) {
+				content.push(`${words[i]} `)
+				isLastWordaPlaceholder = false
+			} else {
+				content[content.length - 1] = content[content.length - 1] + `${words[i]} `
+			}
+		}
+
+		return content
+	}
+
 	const dismissItem = ev => {
 		dismissCallback?.()
 		ev.stopPropagation()
@@ -39,22 +83,9 @@ const NotificationRow = memo(function NotificationRow({
 			<Icon className={styles.dismissIcon} iconName='Cancel' onClick={ev => dismissItem(ev)} />
 			{mention?.createdBy ? (
 				<>
-					<div className='mb-2'>
-						<span className='text-primary'>
-							{mention.createdBy.name.first} {mention.createdBy.name.last}
-						</span>{' '}
-						mentioned you in{' '}
-						<span className={styles.description}>
-							<ShortString
-								text={mention?.engagement?.description}
-								limit={30}
-								showReadMoreLess={false}
-							/>
-						</span>{' '}
-						request.
-					</div>
+					<div className='mb-2'>{getNotificationItemBody()}</div>
 					{mention?.message && (
-						<div className={cx('mb-3', styles.comment)}>"{mention.message}"</div>
+						<div className={cx('mb-3', styles.comment)}>&ldquo;{mention.message}&rdquo;</div>
 					)}
 				</>
 			) : (
