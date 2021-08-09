21ae4dc3b9d1cff4f1961d83e7f3e788b286a6df
Devan Huapaya <devan@genui.com>
2021-08-09 14:27:21 -0700

    Converted service-worker to typescript, and added web.config

--- packages/webapp/public/sw.js
+++
@@ -1,8 +0,0 @@
-/*!
-* Copyright (c) Microsoft. All rights reserved.
-* Licensed under the MIT license. See LICENSE file in the project.
-*/
-
-self.addEventListener("install", function (event) {
-  console.log("Service Worker installed 🤙");
-});
