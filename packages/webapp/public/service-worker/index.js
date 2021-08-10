5f21168527321b28276996a88697792483a1c9f0
Devan Huapaya <devan@genui.com>
2021-08-10 12:28:26 -0700

    Refactored service worker to js and updated manifest.json

---
+++ packages/webapp/public/service-worker/index.js
@@ -0,0 +1,9 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+
+/* eslint-disable-next-line no-restricted-globals */
+self.addEventListener('install', (event) => {
+	console.log('Service Worker installed...', event)
+})
