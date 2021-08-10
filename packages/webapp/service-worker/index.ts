90737068bde7818a1217c52a2e5118259ffe63c1
Devan Huapaya <devan@genui.com>
2021-08-09 23:32:39 -0700

    Updated service worker tsconfig

--- packages/webapp/service-worker/index.ts
+++ packages/webapp/service-worker/index.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-
 const installEvent: EventListener = event => {
 	console.log('Service Worker installed 🤙', event)
 }
