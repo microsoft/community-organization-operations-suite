c701ca6bbc2dd3f7872758660a269b17c90d05a4
Devan Huapaya <devan@genui.com>
2021-08-10 10:57:57 -0700

    Added config to service worker and updated tsconfig

--- packages/webapp/service-worker/index.ts
+++ packages/webapp/service-worker/index.ts
@@ -2,11 +2,14 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-const installEvent: EventListener = event => {
-	console.log('Service Worker installed 🤙', event)
-}
+import conf from 'config'
+import { Configuration } from './Configuration'
 
+const config = new Configuration(conf)
+
+const installEvent: EventListener = event => {
+	console.log('Service Worker installed...', event)
+	console.log('Config key in service-worker', config)
+}
 /* eslint-disable-next-line no-restricted-globals */
 self.addEventListener('install', installEvent)
-
-export {}
