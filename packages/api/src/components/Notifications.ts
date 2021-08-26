5a38d9fc2710fad8aaf0678583c5e468f76ec488
Chris Trevino <chtrevin@microsoft.com>
2021-08-26 14:47:15 -0700

    lint

--- packages/api/src/components/Notifications.ts
+++ packages/api/src/components/Notifications.ts
@@ -28,7 +28,7 @@ export class Notifications {
 
 	public constructor(config: Configuration) {
 		this.#config = config
-		const isEnabled: boolean = Boolean(config.firebaseSettings.private_key)
+		const isEnabled = Boolean(config.firebaseSettings.private_key)
 		this.#fbAdmin = isEnabled
 			? fbInitializeApp({
 					credential: fbCredential.cert(config.firebaseSettings as FBServiceAccount)
