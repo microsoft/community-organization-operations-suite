56f1410d8ce3dbbb131620c8e3ff8fb991683f39
Chris Trevino <chtrevin@microsoft.com>
2021-08-31 13:54:01 -0700

    move next.js assetPrefix into config stack

--- packages/webapp/next.config.js
+++ packages/webapp/next.config.js
@@ -2,15 +2,13 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-
 // This file is not going through babel transformation.
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
-
-const debug = process.env.NODE_ENV !== 'production'
+const config = require('config')
 
 module.exports = {
-	assetPrefix: !debug ? '' : '',
+	assetPrefix: config.get('next.assetPrefix'),
 	trailingSlash: true,
 	future: {
 		webpack5: true
