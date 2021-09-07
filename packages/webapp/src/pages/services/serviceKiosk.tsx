2e9d0347fe6ffafef089e2bd1a69c5cebf015d0f
Batch Mosqueda <batch@genui.com>
2021-09-07 11:23:13 -0700

    added form reload on submit

--- packages/webapp/src/pages/services/serviceKiosk.tsx
+++ packages/webapp/src/pages/services/serviceKiosk.tsx
@@ -3,7 +3,7 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import ContainerLayout from '~layouts/ContainerLayout'
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import ClientOnly from '~ui/ClientOnly'
 import { useServiceList } from '~hooks/api/useServiceList'
 import { useCurrentUser } from '~hooks/api/useCurrentUser'
@@ -17,24 +17,32 @@ const EditService = memo(function EditService(): JSX.Element {
 	const { t } = useTranslation('services')
 	const router = useRouter()
 	const { serviceList, addServiceAnswer } = useServiceList(orgId)
+	const [showForm, setShowForm] = useState(true)
 
 	const { sid } = router.query
 	const selectedService =
 		typeof sid === 'string' ? serviceList.find((s) => s.id === sid) : undefined
 
 	const handleAddServiceAnswer = async (values) => {
-		await addServiceAnswer(values)
+		const res = await addServiceAnswer(values)
+		if (res) {
+			// Note: need a better way to do this
+			setShowForm(false)
+			setShowForm(true)
+		}
 	}
 
 	return (
 		<ContainerLayout documentTitle={t('pageTitle')}>
 			<ClientOnly>
 				<div className='mt-5'>
-					<FormGenerator
-						service={selectedService}
-						onSubmit={handleAddServiceAnswer}
-						previewMode={false}
-					/>
+					{showForm && (
+						<FormGenerator
+							service={selectedService}
+							onSubmit={handleAddServiceAnswer}
+							previewMode={false}
+						/>
+					)}
 				</div>
 			</ClientOnly>
 		</ContainerLayout>
