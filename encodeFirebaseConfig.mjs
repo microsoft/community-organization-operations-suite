c7a740922d44c41a6c622fe08ab2efdd45796211
Chris Trevino <chtrevin@microsoft.com>
2021-09-17 14:19:16 -0700

    fix: use a base64 encoded firebase config document

---
+++ encodeFirebaseConfig.mjs
@@ -0,0 +1,19 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import fs from 'fs'
+import os from 'os'
+
+const input = process.argv[2]
+if (input == null) {
+	throw new Error(`path to firebase config must be provided`)
+}
+const filename = input.replace(/^~/, os.homedir())
+const cert = fs.readFileSync(filename, 'utf8')
+
+function convertToBase64(text) {
+	const buff = Buffer.from(JSON.stringify(text), 'utf8')
+	return buff.toString('base64')
+}
+console.log(convertToBase64(cert))
