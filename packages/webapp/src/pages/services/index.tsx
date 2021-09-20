637e64c477f64590662f31220e55afbd21adbb06
Batch Mosqueda <batch@genui.com>
2021-09-20 10:08:08 -0700

    updated service archive modal

--- packages/webapp/src/pages/services/index.tsx
+++ packages/webapp/src/pages/services/index.tsx
@@ -3,21 +3,19 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import ContainerLayout from '~layouts/ContainerLayout'
-import { memo, useRef } from 'react'
+import { memo, useRef, useState } from 'react'
 import ServiceList from '~components/lists/ServiceList'
 import { useServiceList } from '~hooks/api/useServiceList'
 import { useCurrentUser } from '~hooks/api/useCurrentUser'
 import { useTranslation } from '~hooks/useTranslation'
 import { Service, ServiceInput, ServiceStatus } from '@cbosuite/schema/dist/client-types'
-import { Dialog, DialogType, DialogFooter } from '@fluentui/react/lib/Dialog'
-import { useBoolean } from '@fluentui/react-hooks'
-import { PrimaryButton, DefaultButton } from '@fluentui/react/lib/Button'
+import ArchiveServiceModal from '~components/ui/ArchiveServiceModal'
 
 const Services = memo(function Services(): JSX.Element {
 	const { orgId } = useCurrentUser()
 	const { t } = useTranslation('services')
 	const { serviceList, loading, updateService } = useServiceList(orgId)
-	const [hideDialog, { toggle: toggleHideDialog }] = useBoolean(true)
+	const [showModal, setShowModal] = useState(false)
 	const serviceInput = useRef(null)
 
 	const handleServiceClose = (values: Service) => {
@@ -29,45 +27,34 @@ const Services = memo(function Services(): JSX.Element {
 			serviceStatus: ServiceStatus.Archive
 		}
 		serviceInput.current = updatedService
-		toggleHideDialog()
+		setShowModal(true)
 	}
 
 	const archiveService = async () => {
 		await updateService(serviceInput.current)
 		serviceInput.current = null
-		toggleHideDialog()
+		setShowModal(false)
 	}
 
 	const serviceName = serviceInput.current ? serviceInput.current.name : ''
 
 	return (
-		<ContainerLayout documentTitle={t('pageTitle')}>
-			<ServiceList
-				title={t('pageTitle')}
-				services={serviceList.filter((s) => s.serviceStatus !== ServiceStatus.Archive)}
-				loading={loading}
-				onServiceClose={handleServiceClose}
+		<>
+			<ContainerLayout documentTitle={t('pageTitle')}>
+				<ServiceList
+					title={t('pageTitle')}
+					services={serviceList.filter((s) => s.serviceStatus !== ServiceStatus.Archive)}
+					loading={loading}
+					onServiceClose={handleServiceClose}
+				/>
+			</ContainerLayout>
+			<ArchiveServiceModal
+				showModal={showModal}
+				serviceName={serviceName}
+				onSubmit={archiveService}
+				onDismiss={() => setShowModal(false)}
 			/>
-			<Dialog
-				hidden={hideDialog}
-				onDismiss={toggleHideDialog}
-				dialogContentProps={{
-					type: DialogType.normal,
-					title: t('archiveModal.title'),
-					closeButtonAriaLabel: t('archiveModal.close'),
-					subText: t('archiveModal.subText', { serviceName })
-				}}
-				modalProps={{
-					isBlocking: false,
-					styles: { main: { maxWidth: 450 } }
-				}}
-			>
-				<DialogFooter>
-					<PrimaryButton onClick={archiveService} text={t('archiveModal.title')} />
-					<DefaultButton onClick={toggleHideDialog} text={t('archiveModal.cancel')} />
-				</DialogFooter>
-			</Dialog>
-		</ContainerLayout>
+		</>
 	)
 })
 export default Services
