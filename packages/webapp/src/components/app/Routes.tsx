8dbd8ca43b83bd9ccd17a87637b9ac1e2cf66771
Chris Trevino <chtrevin@microsoft.com>
2021-09-27 13:33:59 -0700

    Add suspense on top of lazy components

--- packages/webapp/src/components/app/Routes.tsx
+++ packages/webapp/src/components/app/Routes.tsx
@@ -3,8 +3,9 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 
-import { FC, lazy, memo } from 'react'
+import { FC, lazy, memo, Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { Spinner } from '@fluentui/react'
 
 const Index = lazy(() => /* webpackChunkName: "IndexPage" */ import('~pages/index'))
 const Account = lazy(() => /* webpackChunkName: "AccountPage" */ import('~pages/account'))
@@ -29,17 +30,19 @@ const ServiceKiosk = lazy(
 
 export const Routes: FC = memo(function Routes() {
 	return (
-		<Switch>
-			<Route exact path='/' component={Index} />
-			<Route path='/account' component={Account} />
-			<Route path='/clients' component={Clients} />
-			<Route path='/login' component={Login} />
-			<Route path='/logout' component={Logout} />
-			<Route path='/passwordReset' component={PasswordReset} />
-			<Route path='/services' component={ServicesIndex} />
-			<Route path='/services/addService' component={AddService} />
-			<Route path='/services/editService' component={EditService} />
-			<Route path='/services/serviceKiosk' component={ServiceKiosk} />
-		</Switch>
+		<Suspense fallback={<Spinner />}>
+			<Switch>
+				<Route exact path='/' component={Index} />
+				<Route path='/account' component={Account} />
+				<Route path='/clients' component={Clients} />
+				<Route path='/login' component={Login} />
+				<Route path='/logout' component={Logout} />
+				<Route path='/passwordReset' component={PasswordReset} />
+				<Route path='/services' component={ServicesIndex} />
+				<Route path='/services/addService' component={AddService} />
+				<Route path='/services/editService' component={EditService} />
+				<Route path='/services/serviceKiosk' component={ServiceKiosk} />
+			</Switch>
+		</Suspense>
 	)
 })
