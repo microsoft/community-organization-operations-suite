6cb8954dcff66c3f1cad8a25ad006efd4c6116f4
Chris Trevino <chtrevin@microsoft.com>
2021-09-30 15:28:08 -0700

    headless mode

--- packages/acceptance-tests/wdio.conf.ts
+++ packages/acceptance-tests/wdio.conf.ts
@@ -62,12 +62,15 @@ const wdioConfig: Options.Testrunner = {
 			maxInstances: 5,
 			//
 			browserName: 'chrome',
-			acceptInsecureCerts: true
+			acceptInsecureCerts: true,
+			'wdio:devtoolsOptions': {
+				headless: true
+			}
 			// If outputDir is provided WebdriverIO can capture driver session logs
 			// it is possible to configure which logTypes to include/exclude.
 			// excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
 			// excludeDriverLogs: ['bugreport', 'server'],
-		}
+		} as any
 	],
 	//
 	// ===================
