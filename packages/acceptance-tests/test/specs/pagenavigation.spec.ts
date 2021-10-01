c5810bb82b221d2f6eb9cbda080e81b7082dc3cf
Chris Trevino <chtrevin@microsoft.com>
2021-10-01 11:05:51 -0700

    acceptance test update

--- packages/acceptance-tests/test/specs/pagenavigation.spec.ts
+++ packages/acceptance-tests/test/specs/pagenavigation.spec.ts
@@ -49,7 +49,7 @@ describe('Top-level page navigation', () => {
 		await ClientsPage.waitForLoad()
 	})
 
-	it.skip('can navigate to tags page', async () => {
+	it('can navigate to tags page', async () => {
 		await Header.clickTags()
 		await TagsPage.waitForLoad()
 	})
@@ -64,7 +64,7 @@ describe('Top-level page navigation', () => {
 		await ProfilePage.waitForLoad()
 	})
 
-	it.skip('can navigate to the 404 page', async () => {
+	it('can navigate to the 404 page', async () => {
 		await NotFoundPage.open()
 		await NotFoundPage.waitForLoad()
 	})
