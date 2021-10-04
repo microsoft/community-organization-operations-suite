4267b324ecc026bb36a4462cc1849ca69a79bd08
Chris Trevino <chtrevin@microsoft.com>
2021-10-04 14:29:55 -0700

    add addTag action to tags page

--- packages/acceptance-tests/test/pageobjects/TagsPage.ts
+++ packages/acceptance-tests/test/pageobjects/TagsPage.ts
@@ -5,11 +5,16 @@
 import { Page } from './Page'
 
 const selectors: Record<string, string> = {
-	tagList: `[data-testid="tag-list"]`
+	tagList: `[data-testid="tag-list"]`,
+	btnAddTag: '.btnAddItem'
 }
 export class TagsPage extends Page {
 	public async waitForLoad() {
 		await super.waitForLoad()
 		await this.page.waitForSelector(selectors.tagList, { state: 'visible' })
+	}
+
+	public async clickAddTag() {
+		await this.page.click(selectors.btnAddTag)
 	}
 }
