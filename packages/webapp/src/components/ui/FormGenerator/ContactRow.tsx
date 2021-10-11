3023dd7a64b3049a6a0c5f1418a7c5aafdb4d1d3
Chris Trevino <chtrevin@microsoft.com>
2021-10-11 16:44:34 -0700

    remove space

--- packages/webapp/src/components/ui/FormGenerator/ContactRow.tsx
+++ packages/webapp/src/components/ui/FormGenerator/ContactRow.tsx
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-
 import { Contact } from '@cbosuite/schema/dist/client-types'
 import { FC, memo, useMemo } from 'react'
 import { Col } from 'react-bootstrap'
