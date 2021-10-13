6e12b059343a3fd54524659e3052041331bcb123
Chris Trevino <chtrevin@microsoft.com>
2021-10-12 18:12:31 -0700

    move useServicePageColumnsBuilder to hook file

--- packages/webapp/src/components/lists/ReportList/index.tsx
+++ packages/webapp/src/components/lists/ReportList/index.tsx
@@ -22,15 +22,8 @@ import { IDropdownOption } from '@fluentui/react'
 import { wrap } from '~utils/appinsights'
 import { Parser } from 'json2csv/dist/json2csv.umd'
 import { useTranslation } from '~hooks/useTranslation'
-import { MultiActionButton, IMultiActionButtons } from '~components/ui/MultiActionButton2'
-import { CLIENT_DEMOGRAPHICS } from '~constants'
 import { useLocale } from '~hooks/useLocale'
 import { DeleteServiceRecordModal } from '~components/ui/DeleteServiceRecordModal'
-import { CustomDateRangeFilter } from '~components/ui/CustomDateRangeFilter'
-import { CustomTextFieldFilter } from '~components/ui/CustomTextFieldFilter'
-import { CustomNumberRangeFilter } from '~components/ui/CustomNumberRangeFilter'
-import { CustomOptionsFilter } from '~components/ui/CustomOptionsFilter'
-import { ShortString } from '~ui/ShortString'
 import { Panel } from '~components/ui/Panel'
 import { useBoolean } from '@fluentui/react-hooks'
 import { FormGenerator } from '~components/ui/FormGenerator'
@@ -40,6 +33,7 @@ import { IFieldFilter, ReportTypes } from './types'
 import { ReportManager } from './ReportManager'
 import { empty } from '~utils/noop'
 import { useClientPageColumnsBuilder } from './useClientPageColumnsBuilder'
+import { useServicePageColumnsBuilder } from './useServicePageColumnsBuilder'
 
 interface ReportListProps {
 	title?: string
@@ -334,9 +328,13 @@ export const ReportList: StandardFC<ReportListProps> = wrap(function ReportList(
 		}
 	}
 
-	const handleDeleteServiceAnswerAction = (serviceAnswer: ServiceAnswers, serviceId: string) => {
+	const handleEditServiceAnswerAction = (service: Service, record: ServiceAnswers) => {
+		setRecordToEdit({ service, record })
+		openEditRecordPanel()
+	}
+	const handleDeleteServiceAnswerAction = (service: Service, record: ServiceAnswers) => {
 		// Save the record to delete and open the confirmation modal
-		setRecordToDelete({ record: serviceAnswer, serviceId })
+		setRecordToDelete({ record: record, serviceId: service.id })
 		setShowModal(true)
 	}
 
@@ -388,257 +386,13 @@ export const ReportList: StandardFC<ReportListProps> = wrap(function ReportList(
 		return headerFilters
 	}, [])
 
-	const buildServicePageColumns = useCallback(
-		(service: Service): IPaginatedListColumn[] => {
-			const customFields = service.customFields ?? []
-
-			const getColumnItemValue = (
-				answerItem: ServiceAnswers,
-				field: ServiceCustomField
-			): string => {
-				let answerValue = ''
-
-				const answers = answerItem.fieldAnswers[field.fieldType]?.find(
-					(a) => a.fieldId === field.fieldId
-				)
-				if (answers) {
-					const fieldValue = customFields.find((f) => f.fieldId === answers.fieldId).fieldValue
-
-					if (Array.isArray(answers.values)) {
-						answerValue = answers.values
-							.map((v) => fieldValue.find((f) => f.id === v).label)
-							.join(', ')
-					} else {
-						switch (field.fieldType) {
-							case 'singleChoice':
-								answerValue = fieldValue.find((f) => f.id === answers.values).label
-								break
-							case 'date':
-								answerValue = new Date(answers.values).toLocaleDateString(locale)
-								break
-							default:
-								answerValue = answers.values
-						}
-					}
-				} else {
-					answerValue = ''
-				}
-
-				return answerValue
-			}
-
-			const _pageColumns: IPaginatedListColumn[] = []
-
-			if (service.contactFormEnabled) {
-				_pageColumns.push(
-					{
-						key: 'name',
-						headerClassName: styles.headerItemCell,
-						itemClassName: styles.itemCell,
-						name: t('clientList.columns.name'),
-						onRenderColumnHeader(key, name, index) {
-							return (
-								<CustomTextFieldFilter
-									filterLabel={name}
-									onFilterChanged={(value) => filterColumnTextValue(key, value)}
-								/>
-							)
-						},
-						onRenderColumnItem(item: ServiceAnswers, index: number) {
-							return `${item?.contacts[0]?.name?.first} ${item?.contacts[0]?.name?.last}`
-						}
-					},
-					{
-						key: 'gender',
-						headerClassName: styles.headerItemCell,
-						itemClassName: styles.itemCell,
-						name: t('demographics.gender.label'),
-						onRenderColumnHeader(key, name, index) {
-							return (
-								<CustomOptionsFilter
-									filterLabel={name}
-									placeholder={name}
-									options={CLIENT_DEMOGRAPHICS[key].options.map((o) => ({
-										key: o.key,
-										text: t(`demographics.${key}.options.${o.key}`)
-									}))}
-									onFilterChanged={(option) => filterColumns(key, option)}
-								/>
-							)
-						},
-						onRenderColumnItem(item: ServiceAnswers, index: number) {
-							return getDemographicValue('gender', item.contacts[0])
-						}
-					},
-					{
-						key: 'race',
-						headerClassName: styles.headerItemCell,
-						itemClassName: styles.itemCell,
-						name: t('demographics.race.label'),
-						onRenderColumnHeader(key, name, index) {
-							return (
-								<CustomOptionsFilter
-									filterLabel={name}
-									placeholder={name}
-									options={CLIENT_DEMOGRAPHICS[key].options.map((o) => ({
-										key: o.key,
-										text: t(`demographics.${key}.options.${o.key}`)
-									}))}
-									onFilterChanged={(option) => filterColumns(key, option)}
-								/>
-							)
-						},
-						onRenderColumnItem(item: ServiceAnswers, index: number) {
-							return getDemographicValue('race', item.contacts[0])
-						}
-					},
-					{
-						key: 'ethnicity',
-						headerClassName: styles.headerItemCell,
-						itemClassName: styles.itemCell,
-						name: t('demographics.ethnicity.label'),
-						onRenderColumnHeader(key, name, index) {
-							return (
-								<CustomOptionsFilter
-									filterLabel={name}
-									placeholder={name}
-									options={CLIENT_DEMOGRAPHICS[key].options.map((o) => ({
-										key: o.key,
-										text: t(`demographics.${key}.options.${o.key}`)
-									}))}
-									onFilterChanged={(option) => filterColumns(key, option)}
-								/>
-							)
-						},
-						onRenderColumnItem(item: ServiceAnswers, index: number) {
-							return getDemographicValue('ethnicity', item.contacts[0])
-						}
-					}
-				)
-			}
-
-			const customFormColumns: IPaginatedListColumn[] = customFields.map((field, index) => ({
-				key: field.fieldId,
-				name: field.fieldName,
-				headerClassName: styles.headerItemCell,
-				itemClassName: styles.itemCell,
-				onRenderColumnHeader(key, name) {
-					const dropdownFieldTypes = ['singleChoice', 'multiChoice']
-					if (dropdownFieldTypes.includes(field.fieldType)) {
-						return (
-							<CustomOptionsFilter
-								filterLabel={name}
-								placeholder={name}
-								options={field.fieldValue.map((value) => ({ key: value.id, text: value.label }))}
-								onFilterChanged={(option) => filterColumns(key, option)}
-							/>
-						)
-					}
-
-					const textFieldFieldTypes = ['singleText', 'multilineText']
-					if (textFieldFieldTypes.includes(field.fieldType)) {
-						return (
-							<CustomTextFieldFilter
-								filterLabel={name}
-								onFilterChanged={(value) => filterColumnTextValue(key, value)}
-							/>
-						)
-					}
-
-					if (field.fieldType === 'date') {
-						return (
-							<CustomDateRangeFilter
-								filterLabel={name}
-								onFilterChanged={({ startDate, endDate }) => {
-									const sDate = startDate ? startDate.toISOString() : ''
-									const eDate = endDate ? endDate.toISOString() : ''
-									filterRangedValues(key, [sDate, eDate])
-								}}
-							/>
-						)
-					}
-
-					if (field.fieldType === 'number') {
-						// get min and max values from service answers
-						let min = 0
-						let max = 0
-						service.answers.forEach((answer) => {
-							answer.fieldAnswers[field.fieldType]?.forEach((fieldAnswer) => {
-								if (fieldAnswer.fieldId === key) {
-									const answerNumber = Number(fieldAnswer.values)
-									if (answerNumber > max) {
-										max = answerNumber
-									}
-									if (answerNumber < min) {
-										min = answerNumber
-									}
-								}
-							})
-						})
-						return (
-							<CustomNumberRangeFilter
-								filterLabel={name}
-								minValue={min}
-								maxValue={max}
-								onFilterChanged={(min, max) => {
-									filterRangedValues(key, [min.toString(), max.toString()])
-								}}
-							/>
-						)
-					}
-				},
-				onRenderColumnItem(item: ServiceAnswers) {
-					if (field.fieldType === 'multilineText') {
-						return <ShortString text={getColumnItemValue(item, field)} limit={50} />
-					}
-					return getColumnItemValue(item, field)
-				}
-			}))
-
-			const actionColumns: IPaginatedListColumn[] = [
-				{
-					key: 'actions',
-					name: '',
-					headerClassName: cx(styles.headerItemCell, styles.actionItemHeader),
-					itemClassName: cx(styles.itemCell, styles.actionItemCell),
-					onRenderColumnItem(item: ServiceAnswers) {
-						const columnActionButtons: IMultiActionButtons<ServiceAnswers>[] = [
-							{
-								name: t('serviceListRowActions.edit'),
-								className: cx(styles.editButton),
-								onActionClick(item) {
-									setRecordToEdit({ service, record: item })
-									openEditRecordPanel()
-								}
-							},
-							{
-								name: t('serviceListRowActions.delete'),
-								className: cx(styles.editButton),
-								onActionClick(item) {
-									handleDeleteServiceAnswerAction(item, service.id)
-								}
-							}
-						]
-						return (
-							<div className={styles.actionItemButtonsWrapper}>
-								<MultiActionButton columnItem={item} buttonGroup={columnActionButtons} />
-							</div>
-						)
-					}
-				}
-			]
-
-			return _pageColumns.concat(customFormColumns).concat(actionColumns)
-		},
-		[
-			t,
-			locale,
-			filterColumnTextValue,
-			filterRangedValues,
-			filterColumns,
-			getDemographicValue,
-			openEditRecordPanel
-		]
+	const buildServicePageColumns = useServicePageColumnsBuilder(
+		filterColumns,
+		filterColumnTextValue,
+		filterRangedValues,
+		getDemographicValue,
+		handleEditServiceAnswerAction,
+		handleDeleteServiceAnswerAction
 	)
 
 	const buildServiceCSVFields = useCallback(
--- packages/webapp/src/components/lists/ReportList/useClientPageColumnsBuilder.tsx
+++ packages/webapp/src/components/lists/ReportList/useClientPageColumnsBuilder.tsx
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-
 import { Contact } from '@cbosuite/schema/dist/client-types'
 import { IDropdownOption } from '@fluentui/react'
 import { useCallback } from 'react'
