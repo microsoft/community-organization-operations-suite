c8b36753af55cc69e7d6f261858466ef01367fe4
Chris Trevino <chtrevin@microsoft.com>
2021-10-15 09:47:07 -0700

    add required field validation to service

--- packages/api/src/interactors/CreateServiceAnswersInteractor.ts
+++ packages/api/src/interactors/CreateServiceAnswersInteractor.ts
@@ -3,12 +3,16 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import {
+	Service,
+	ServiceAnswer,
+	ServiceAnswerField,
 	ServiceAnswerInput,
 	ServiceAnswerResponse,
+	ServiceFieldRequirement,
 	StatusType
 } from '@cbosuite/schema/dist/provider-types'
 import { Localization } from '~components'
-import { ServiceAnswerCollection } from '~db'
+import { DbService, DbServiceField, ServiceAnswerCollection, ServiceCollection } from '~db'
 import { createDBServiceAnswer } from '~dto'
 import { createGQLServiceAnswer } from '~dto/createGQLServiceAnswer'
 import { Interactor } from '~types'
@@ -17,10 +21,16 @@ export class CreateServiceAnswersInteractor
 	implements Interactor<ServiceAnswerInput, ServiceAnswerResponse>
 {
 	#localization: Localization
+	#services: ServiceCollection
 	#serviceAnswers: ServiceAnswerCollection
 
-	public constructor(localization: Localization, serviceAnswers: ServiceAnswerCollection) {
+	public constructor(
+		localization: Localization,
+		services: ServiceCollection,
+		serviceAnswers: ServiceAnswerCollection
+	) {
 		this.#localization = localization
+		this.#services = services
 		this.#serviceAnswers = serviceAnswers
 	}
 
@@ -32,6 +42,16 @@ export class CreateServiceAnswersInteractor
 				status: StatusType.Failed
 			}
 		}
+		const service = await this.#services.itemById(answer.serviceId)
+		if (!service.item) {
+			return {
+				serviceAnswer: null,
+				message: this.#localization.t('mutation.createServiceAnswers.serviceNotFound'),
+				status: StatusType.Failed
+			}
+		}
+
+		validateAnswer(service.item, answer)
 
 		const dbServiceAnswer = createDBServiceAnswer(answer)
 		this.#serviceAnswers.insertItem(dbServiceAnswer)
@@ -42,4 +62,24 @@ export class CreateServiceAnswersInteractor
 			status: StatusType.Success
 		}
 	}
+}
+
+function validateAnswer(service: DbService, answer: ServiceAnswerInput) {
+	const serviceFieldHash: Record<string, DbServiceField> = {}
+	const answerFieldHash: Record<string, ServiceAnswerField> = {}
+	service.fields?.forEach((field) => {
+		serviceFieldHash[field.id] = field
+	})
+	answer.fields.forEach((field) => {
+		answerFieldHash[field.fieldId] = field
+	})
+
+	service.fields?.forEach((f) => {
+		// Validate that all required fields have been submitted
+		if (f.requirement === ServiceFieldRequirement.Required) {
+			if (!answerFieldHash[f.id]) {
+				throw new Error(`Missing required field ${f.id} for service ${service.id}`)
+			}
+		}
+	})
 }
