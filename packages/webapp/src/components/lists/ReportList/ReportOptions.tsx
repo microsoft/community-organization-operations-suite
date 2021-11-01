c48f7671440d2582c3cac19ebb2bf88940d36ef3
Chris Trevino <chtrevin@microsoft.com>
2021-11-01 14:00:24 -0700

    remove unused import

--- packages/webapp/src/components/lists/ReportList/ReportOptions.tsx
+++ packages/webapp/src/components/lists/ReportList/ReportOptions.tsx
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { Text } from '@fluentui/react'
 import cx from 'classnames'
 import { FC, memo, useCallback } from 'react'
 import { Col, Row } from 'react-bootstrap'
