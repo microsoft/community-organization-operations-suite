c005a11f468117be19fcdc079d670072f405fa28
Chris Trevino <chtrevin@microsoft.com>
2021-11-01 09:46:58 -0700

    update migration scripts to import reflect-metadata

--- packages/api/migrateMongo.ts
+++ packages/api/migrateMongo.ts
@@ -3,63 +3,10 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 /* eslint-disable @essex/adjacent-await */
-require('dotenv').config()
-const { Migrator } = require('./src/components/Migrator')
-const { createLogger } = require('./src/utils')
-const appConfig = require('config')
-const { Configuration } = require('./src/components/Configuration')
-const logger = createLogger('migrator', true)
+import 'reflect-metadata'
+import { config } from 'dotenv'
 
-/**
- * Wrapper around migrate-mongo to enable typescript support and load config api components
- */
-async function migration() {
-	const [, , command, migrationNameToCreate] = process.argv
-	const config = new Configuration(appConfig)
-	logger(
-		`launching migrator env="${
-			process.env.NODE_CONFIG_ENV || process.env.NODE_ENV
-		}"; connectionString="${config.dbConnectionString}"`
-	)
-	const migrator = new Migrator(config)
-	await migrator.connect()
+// configure environment
+config()
 
-	switch (command) {
-		case 'init':
-			await migrator.init()
-			logger('Initialized migrate-mongo')
-			break
-		case 'create':
-			const migration = await migrator.create(migrationNameToCreate)
-			logger('Created migration', migration)
-			break
-		case 'up':
-			await migrator.up()
-			break
-		case 'down':
-			await migrator.down()
-			break
-		case 'status':
-			const status = await migrator.status()
-			logger(status)
-			break
-		default:
-			logger('No command provided to migrate-mongo')
-			logger('Please use one of the following commands:')
-			logger('- create <name_of_migration>')
-			logger('- up')
-			logger('- down')
-			logger('- status')
-			break
-	}
-}
-
-migration()
-	.then(() => {
-		logger('finished migrations')
-		process.exit(0)
-	})
-	.catch((err) => {
-		logger(err)
-		process.exit(1)
-	})
+import('./scripts/migrate')
---
+++ packages/api/scripts/migrate.ts
@@ -0,0 +1,63 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+/* eslint-disable @essex/adjacent-await */
+import { Migrator } from '../src/components/Migrator'
+import { createLogger } from '../src/utils'
+import { Configuration } from '../src/components/Configuration'
+const logger = createLogger('migrator', true)
+
+/**
+ * Wrapper around migrate-mongo to enable typescript support and load config api components
+ */
+async function migration() {
+	const [, , command, migrationNameToCreate] = process.argv
+	const config = new Configuration()
+	logger(
+		`launching migrator env="${
+			process.env.NODE_CONFIG_ENV || process.env.NODE_ENV
+		}"; connectionString="${config.dbConnectionString}"`
+	)
+	const migrator = new Migrator(config)
+	await migrator.connect()
+
+	switch (command) {
+		case 'init':
+			await migrator.init()
+			logger('Initialized migrate-mongo')
+			break
+		case 'create':
+			const migration = await migrator.create(migrationNameToCreate)
+			logger('Created migration', migration)
+			break
+		case 'up':
+			await migrator.up()
+			break
+		case 'down':
+			await migrator.down()
+			break
+		case 'status':
+			const status = await migrator.status()
+			logger(status)
+			break
+		default:
+			logger('No command provided to migrate-mongo')
+			logger('Please use one of the following commands:')
+			logger('- create <name_of_migration>')
+			logger('- up')
+			logger('- down')
+			logger('- status')
+			break
+	}
+}
+
+migration()
+	.then(() => {
+		logger('finished migrations')
+		process.exit(0)
+	})
+	.catch((err) => {
+		logger(err)
+		process.exit(1)
+	})
