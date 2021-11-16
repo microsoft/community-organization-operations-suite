2a58e86b62a9e9f8a23260c7c5dea292ef788e43
Devan Huapaya <devan@genui.com>
2021-11-16 11:35:00 -0800

    Resolved export issue

--- packages/webapp/src/components/lists/ReportList/useCsvExport.ts
+++ packages/webapp/src/components/lists/ReportList/useCsvExport.ts
@@ -3,20 +3,18 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 
-import { useCallback, useRef } from 'react'
+import { useCallback, useState } from 'react'
 import { empty } from '~utils/noop'
 import { CsvField } from './types'
 import { Parser } from 'json2csv/dist/json2csv.umd'
 import { downloadFile } from '~utils/downloadFile'
 
 export function useCsvExport(data: unknown[]) {
-	const csvFields = useRef<CsvField[]>(empty)
-	const setCsvFields = useCallback((a: CsvField[]) => {
-		csvFields.current = a
-	}, [])
+	const [csvFields, setCsvFields] = useState<CsvField[]>(empty)
+
 	const downloadCSV = useCallback(
 		function downloadCSV() {
-			const fields = csvFields.current
+			const fields = csvFields
 			const parser = new Parser({ fields })
 			const csv = parser.parse(data)
 			const csvData = new Blob([csv], { type: 'text/csv' })
