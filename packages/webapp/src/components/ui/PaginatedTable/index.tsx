7793c329ff178f5711b2b9b5c81cd81d93ea01c0
Matthias Courville <matthias@genui.com>
2022-01-10 08:51:55 -0800

    Added UX indicator for table horizontal scroll

--- packages/webapp/src/components/ui/PaginatedTable/index.tsx
+++ packages/webapp/src/components/ui/PaginatedTable/index.tsx
@@ -50,8 +50,10 @@ export const PaginatedTable = memo(function PaginatedTable<T>({
 	onPageChange = noop
 }: PaginatedTableProps<T>): JSX.Element {
 	const { c } = useTranslation()
-	const paginatorWrapper = useRef()
+	const paginatorContainer = useRef()
 	const [overflowActive, setOverflowActive] = useState(false)
+	const [leftScrollPocketActive, setLeftScrollPocketActive] = useState(false)
+	const [rightScrollPocketActive, setRightScrollPocketActive] = useState(false)
 
 	const renderColumnItem = (column: IPaginatedTableColumn, item, index): JSX.Element => {
 		if (Array.isArray(column.name)) {
@@ -78,9 +80,18 @@ export const PaginatedTable = memo(function PaginatedTable<T>({
 	}
 
 	const isOverflowActive = useCallback(() => {
-		const element = paginatorWrapper.current as any
+		const element = paginatorContainer.current as any
 		return element.offsetHeight < element.scrollHeight || element.offsetWidth < element.scrollWidth
 	}, [])
+
+	const onScroll = () => {
+		const wrapper = paginatorContainer.current
+		setLeftScrollPocketActive(wrapper.scrollLeft > 0)
+		setRightScrollPocketActive(
+			wrapper.children[0].offsetWidth !== wrapper.offsetWidth &&
+				wrapper.scrollLeft < wrapper.children[0].offsetWidth - wrapper.offsetWidth
+		)
+	}
 
 	useEffect(() => {
 		if (isOverflowActive()) {
@@ -88,6 +99,10 @@ export const PaginatedTable = memo(function PaginatedTable<T>({
 		} else {
 			setOverflowActive(false)
 		}
+		paginatorContainer.current.addEventListener('scroll', onScroll)
+		onScroll()
+
+		return () => window.removeEventListener('scroll', onScroll)
 	}, [list, columns, isOverflowActive])
 
 	return (
@@ -95,77 +110,99 @@ export const PaginatedTable = memo(function PaginatedTable<T>({
 			<Col className={cx(isMD ? null : 'ps-2')}>
 				<Row className={cx('mb-3', 'align-items-end')}></Row>
 			</Col>
-			<Col
-				ref={paginatorWrapper}
-				className={cx(overflowActive ? paginatorContainerClassName : null)}
-			>
-				<div className={cx(styles.table, tableClassName)}>
-					<div className={styles.tableHeaders}>
-						<div className={cx(styles.tableHeadersRow, headerRowClassName)}>
-							{columns?.map(
-								(
-									{
-										key,
-										name,
-										headerClassName,
-										onRenderColumnHeader = nullFn
-									}: IPaginatedTableColumn,
-									index: number
-								) => {
-									return (
-										<div key={index} className={cx(styles.tableHeadersCell, headerClassName)}>
-											{onRenderColumnHeader(key, name, index) || name}
-										</div>
-									)
-								}
-							)}
+			<div className={styles.paginatorWrapper}>
+				<Col
+					ref={paginatorContainer}
+					className={cx(overflowActive ? paginatorContainerClassName : null)}
+				>
+					<div className={cx(styles.table, tableClassName)}>
+						<div className={styles.tableHeaders}>
+							<div className={cx(styles.tableHeadersRow, headerRowClassName)}>
+								{columns?.map(
+									(
+										{
+											key,
+											name,
+											headerClassName,
+											onRenderColumnHeader = nullFn
+										}: IPaginatedTableColumn,
+										index: number
+									) => {
+										return (
+											<div key={index} className={cx(styles.tableHeadersCell, headerClassName)}>
+												{onRenderColumnHeader(key, name, index) || name}
+											</div>
+										)
+									}
+								)}
+							</div>
 						</div>
-					</div>
-					<Paginator
-						isLoading={isLoading}
-						list={list}
-						itemsPerPage={itemsPerPage}
-						onPageChange={onPageChange}
-						controlClass={cx(list.length <= itemsPerPage ? styles.noPaginator : styles.paginator)}
-						loadingItem={() => {
-							return (
-								<div className={styles.loadingWrapper}>
-									<div className={styles.loadingSpinner}>
-										<Spinner className='waitSpinner' size={1} />
-										<span>{c('paginatedList.loading')}</span>
+						<Paginator
+							isLoading={isLoading}
+							list={list}
+							itemsPerPage={itemsPerPage}
+							onPageChange={onPageChange}
+							controlClass={cx(list.length <= itemsPerPage ? styles.noPaginator : styles.paginator)}
+							loadingItem={() => {
+								return (
+									<div className={styles.loadingWrapper}>
+										<div className={styles.loadingSpinner}>
+											<Spinner className='waitSpinner' size={1} />
+											<span>{c('paginatedList.loading')}</span>
+										</div>
 									</div>
-								</div>
-							)
-						}}
-						paginatedListContainerClass={list.length > 0 ? styles.tableBody : styles.noTableBody}
-						renderList={(items: T[]) => (
-							<>
-								{pageItems(list, items).length > 0
-									? pageItems(list, items).map((item: T, id: number) => {
-											return (
-												<div key={id} className={cx(styles.tableBodyRow, bodyRowClassName)}>
-													{columns?.map((column: IPaginatedTableColumn, index: number) => {
-														return (
-															<div
-																key={index}
-																className={cx(styles.tableBodyCell, column.itemClassName)}
-															>
-																{column.onRenderColumnItem(item, index) ??
-																	renderColumnItem(column, item, index)}
-															</div>
-														)
-													})}
-												</div>
-											)
-									  })
-									: columns.length > 0 && (
-											<div className={styles.noResults}>{c('paginatedList.noResults')}</div>
-									  )}
-							</>
-						)}
-					/>
+								)
+							}}
+							paginatedListContainerClass={list.length > 0 ? styles.tableBody : styles.noTableBody}
+							renderList={(items: T[]) => (
+								<>
+									{pageItems(list, items).length > 0
+										? pageItems(list, items).map((item: T, id: number) => {
+												return (
+													<div key={id} className={cx(styles.tableBodyRow, bodyRowClassName)}>
+														{columns?.map((column: IPaginatedTableColumn, index: number) => {
+															return (
+																<div
+																	key={index}
+																	className={cx(styles.tableBodyCell, column.itemClassName)}
+																>
+																	{column.onRenderColumnItem(item, index) ??
+																		renderColumnItem(column, item, index)}
+																</div>
+															)
+														})}
+													</div>
+												)
+										  })
+										: columns.length > 0 && (
+												<div className={styles.noResults}>{c('paginatedList.noResults')}</div>
+										  )}
+								</>
+							)}
+						/>
+					</div>
+				</Col>
+				<div
+					className={cx(
+						styles.scrollPocket,
+						styles.scrollPocketLeft,
+						leftScrollPocketActive ? styles.scrollPocketActive : null
+					)}
+				>
+					<div className={cx(styles.scrollPocketShadow, styles.scrollPocketShadowLeft)}></div>
+					<div className={cx(styles.scrollPocketCaret, styles.scrollPocketCaretLeft)}></div>
 				</div>
-			</Col>
+				<div
+					className={cx(
+						styles.scrollPocket,
+						styles.scrollPocketRight,
+						rightScrollPocketActive ? styles.scrollPocketActive : null
+					)}
+				>
+					<div className={cx(styles.scrollPocketShadow, styles.scrollPocketShadowRight)}></div>
+					<div className={cx(styles.scrollPocketCaret, styles.scrollPocketCaretRight)}></div>
+				</div>
+			</div>
 		</div>
 	)
 })
