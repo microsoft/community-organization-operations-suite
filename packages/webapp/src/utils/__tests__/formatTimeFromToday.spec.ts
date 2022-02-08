b33e66f6cd24c18b6ca37865d960110b6e3d37cf
phorne <phorne@uncharted.software>
2022-02-08 15:20:51 -0500

    Pass in proper type to time formatter.

--- packages/webapp/src/utils/__tests__/formatTimeFromToday.spec.ts
+++ packages/webapp/src/utils/__tests__/formatTimeFromToday.spec.ts
@@ -17,18 +17,18 @@ const setHour = (date: Date, hour: number): Date => {
 describe('The today time formatter', () => {
 	it('will return Today if timestamp is today', () => {
 		const testDate = setHour(new Date(), 8)
-		const parsed = formatTimeFromToday(testDate)
+		const parsed = formatTimeFromToday(testDate.toString())
 		expect(parsed).toBe('Today at 8:00 AM')
 	})
 	it('will return Yesterday if timestamp is yesterday', () => {
 		const testDate = setHour(new Date(), 8)
 		testDate.setDate(testDate.getDate() - 1)
-		const parsed = formatTimeFromToday(testDate)
+		const parsed = formatTimeFromToday(testDate.toString())
 		expect(parsed).toBe('Yesterday at 8:00 AM')
 	})
 	it('will return date string if timestamp is not today nor yesterday', () => {
 		const testDate = setHour(new Date('2001-02-03 11:00'), 8)
-		const parsed = formatTimeFromToday(testDate)
+		const parsed = formatTimeFromToday(testDate.toString())
 		expect(parsed).toBe('Sat Feb 03 2001 at 8:00 AM')
 	})
 })
