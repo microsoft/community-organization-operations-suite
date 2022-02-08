ad8c7418b41e5127fb8caf76f86e785de7d7e85a
phorne <phorne@uncharted.software>
2022-02-08 16:18:06 -0500

    Empty commit to trigger jobs.

--- packages/webapp/src/utils/__tests__/getLocationQuery.spec.ts
+++ packages/webapp/src/utils/__tests__/getLocationQuery.spec.ts
@@ -9,23 +9,23 @@ describe('The location query', () => {
 	it('can parse a single search parameter', () => {
 		const rec = getLocationQuery('test=abc')
 		expect(rec['test']).toBe('abc')
-		expect(Object.keys(rec).length).toBe(1)
+		expect(Object.keys(rec)).toHaveLength(1)
 	})
-	it('can parse multiple search parameter', () => {
+	it('can parse multiple search parameters', () => {
 		const rec = getLocationQuery('test=abc&second=45&third=last')
-		expect(Object.keys(rec).length).toBe(3)
+		expect(Object.keys(rec)).toHaveLength(3)
 		expect(rec['test']).toBe('abc')
 		expect(rec['second']).toBe('45')
 		expect(rec['third']).toBe('last')
 	})
 	it('only retains one value for the same key', () => {
 		const rec = getLocationQuery('test=abc&test=def&test=zzz')
-		expect(Object.keys(rec).length).toBe(1)
+		expect(Object.keys(rec)).toHaveLength(1)
 		expect(rec['test']).toBe('abc')
 	})
 	it('handles empty strings', () => {
 		const rec = getLocationQuery('')
-		expect(Object.keys(rec).length).toBe(1)
-		expect(rec['']).toBe(undefined)
+		expect(Object.keys(rec)).toHaveLength(1)
+		expect(rec['']).toBeUndefined()
 	})
 })
