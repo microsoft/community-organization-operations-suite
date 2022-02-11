a1398484ebad9847dfd0d92165a2b22e8c327372
Yohann Paris <github@yohannparis.com>
2022-02-11 10:40:12 -0500

    Remove shadow from footer

--- packages/webapp/src/components/ui/Footer/index.tsx
+++ packages/webapp/src/components/ui/Footer/index.tsx
@@ -11,84 +11,37 @@ import { constants } from '~utils/features'
 import { useWindowSize } from '~hooks/useWindowSize'
 
 export const Footer: StandardFC = memo(function Footer(_props) {
-	const dims = useWindowSize()
-	return dims.isLessThanLG ? <FooterMobile /> : <FooterDesktop />
-})
-
-function FooterMobile() {
-	return (
-		<>
-			<div className={styles.footerMobile}>
-				<FooterLinks />
-			</div>
-		</>
-	)
-}
-
-function FooterDesktop() {
-	return (
-		<div className={styles.footerContainer}>
-			<div className={styles.footer}>
-				<FooterLinks join={' | '} />
-			</div>
-		</div>
-	)
-}
-
-const FooterLinks: FC<{ join?: string }> = memo(function FooterLinks({ join }) {
 	const { t } = useTranslation(Namespace.Footer)
+	const classname = useWindowSize().isLessThanLG ? styles.footerMobile : styles.footer
+
 	const links = [
-		constants.privacyUrl ? (
-			<Link key='privacy' href={constants.privacyUrl}>
+		!!constants.privacyUrl && (
+			<a key='privacy' href={constants.privacyUrl} rel='noreferrer'>
 				{t('footerBar.privacyAndCookies')}
-			</Link>
-		) : null,
-		constants.trademarksUrl ? (
-			<Link key='trademarks' href={constants.trademarksUrl}>
+			</a>
+		),
+		!!constants.trademarksUrl && (
+			<a key='trademarks' href={constants.trademarksUrl} rel='noreferrer'>
 				{t('footerBar.trademarks')}
-			</Link>
-		) : null,
-		constants.termsOfUseUrl ? (
-			<Link key='termsofuse' href={constants.termsOfUseUrl}>
+			</a>
+		),
+		!!constants.termsOfUseUrl && (
+			<a key='termsofuse' href={constants.termsOfUseUrl} rel='noreferrer'>
 				{t('footerBar.termsOfUse')}
-			</Link>
-		) : null,
-		constants.contactUsEmail ? (
-			<Link key='contactus' href={`mailto:${constants.contactUsEmail}`}>
+			</a>
+		),
+		!!constants.contactUsEmail && (
+			<a key='contactus' href={`mailto:${constants.contactUsEmail}`} rel='noreferrer'>
 				{t('footerBar.contactUs')}
-			</Link>
-		) : null,
-		constants.codeOfConductUrl ? (
-			<Link key='codeofconduct' href={constants.codeOfConductUrl}>
+			</a>
+		),
+		!!constants.codeOfConductUrl && (
+			<a key='codeofconduct' href={constants.codeOfConductUrl} rel='noreferrer'>
 				{t('footerBar.codeOfConduct')}
-			</Link>
-		) : null,
-		constants.copyright ? <Link key='copyright'>{constants.copyright}</Link> : null
-	].filter((t) => !!t)
-	const elements: Array<JSX.Element | string> = []
-	for (let i = 0; i < links.length; i++) {
-		elements.push(links[i])
-		if (i < links.length - 1) {
-			elements.push(join)
-		}
-	}
-	return <>{elements}</>
-})
+			</a>
+		),
+		constants.copyright && <span key='copyright'>{constants.copyright}</span>
+	].filter((link) => !!link)
 
-const Link: FC<{
-	href?: string
-	className?: string
-	style?: CSSProperties
-}> = memo(function Link({ className, children, href, style }) {
-	const finalClassName = classnames(styles.link, { className })
-
-	return href == null ? (
-		<div style={style} className={finalClassName}>
-			{children}
-		</div>
-	) : (
-		<a target='_blank' rel='noreferrer' href={href} style={style} className={finalClassName}>
-			{children}
-		</a>
-	)
+	return <footer className={classname}>{links}</footer>
 })
