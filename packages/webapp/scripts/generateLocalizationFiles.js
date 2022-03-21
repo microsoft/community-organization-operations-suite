e06203d63a946b9991300570803d8c52e165be22
phorne <phorne@uncharted.software>
2022-03-21 09:39:07 -0400

    Added exception to console rule for localization script.

--- packages/webapp/scripts/generateLocalizationFiles.js
+++ packages/webapp/scripts/generateLocalizationFiles.js
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+/* eslint-disable no-console */
 const fs = require('fs')
 const path = require('path')
 
