3b8df0f83db22af7d2173991b7b886ba8311a7e7
Yohann Paris <github@yohannparis.com>
2022-04-06 15:06:35 -0400

    Adding tag on the login page

--- packages/webapp/src/components/ui/LoginPageBody/index.tsx
+++ packages/webapp/src/components/ui/LoginPageBody/index.tsx
@@ -20,7 +20,6 @@ import { StatusType } from '~hooks/api'
 export const LoginPageBody: StandardFC = memo(function LoginPageBody({ children }) {
 	const { t } = useTranslation(Namespace.Login)
 	const { isMD } = useWindowSize()
-	const rounded = isMD ? styles.formContainer : styles.formContainerNoRounded
 	const history = useHistory()
 	const handleLogin = useCallback(
 		(status: string) => {
@@ -47,7 +46,7 @@ export const LoginPageBody: StandardFC = memo(function LoginPageBody({ children 
 									<h1 className='mb-5'>{t('header')}</h1>
 									<p className={styles.subHeader}>{t('subHeader')}</p>
 								</Col>
-								<Col className={cx('shadow', rounded)}>
+								<Col className={cx(styles.formContainer, isMD && 'formContainerRounded')}>
 									<LoginForm onLoginClick={handleLogin} error={error} />
 								</Col>
 							</Row>
