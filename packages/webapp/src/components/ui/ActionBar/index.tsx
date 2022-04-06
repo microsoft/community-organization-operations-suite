926f0bec8d803394abf9bbd5495b13ffbe38bf1b
Yohann Paris <github@yohannparis.com>
2022-04-06 13:41:49 +0000

    Remove ui/Modal used nowhere.

--- packages/webapp/src/components/ui/ActionBar/index.tsx
+++ packages/webapp/src/components/ui/ActionBar/index.tsx
@@ -2,10 +2,9 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { Icon, Link } from '@fluentui/react'
+import { Link } from '@fluentui/react'
 import cx from 'classnames'
-import { isValidElement, memo, useCallback } from 'react'
-import { Button } from 'react-bootstrap'
+import { isValidElement, memo } from 'react'
 import styles from './index.module.scss'
 import { useWindowSize } from '~hooks/useWindowSize'
 import type { StandardFC } from '~types/StandardFC'
@@ -15,17 +14,14 @@ import { TopNav } from '~ui/TopNav'
 import { Notifications } from '~ui/Notifications'
 import { LanguageDropdown } from '../LanguageDropdown'
 import { useTranslation } from '~hooks/useTranslation'
-import { useHistory } from 'react-router-dom'
 
 export interface ActionBarProps {
 	showNav?: boolean
-	showBack?: boolean
 	showTitle?: boolean
 	showPersona?: boolean
 	showNotifications?: boolean
 	title?: string | JSX.Element
 	size?: 'sm' | 'md' | 'lg'
-	onBack?: () => void
 }
 
 /**
@@ -34,23 +30,13 @@ export interface ActionBarProps {
 export const ActionBar: StandardFC<ActionBarProps> = memo(function ActionBar({
 	children,
 	showNav = false,
-	showBack = false,
 	showTitle = false,
 	showPersona = false,
 	showNotifications = false,
 	size,
-	onBack,
 	title
 }) {
 	const { isLG } = useWindowSize()
-	const history = useHistory()
-	const handleBackClick = useCallback(() => {
-		if (onBack) {
-			onBack()
-		} else {
-			history.goBack()
-		}
-	}, [history, onBack])
 	const { c } = useTranslation()
 
 	return (
@@ -63,15 +49,6 @@ export const ActionBar: StandardFC<ActionBarProps> = memo(function ActionBar({
 			<CRC size={size}>
 				<div className='d-flex justify-content-between align-items-center'>
 					<div className='d-flex align-items-center'>
-						{showBack && (
-							<Button
-								className='btn-link text-light d-flex align-items-center text-decoration-none ps-0 pointer'
-								onClick={handleBackClick}
-							>
-								<Icon className='me-2' iconName='ChevronLeft' /> Back
-							</Button>
-						)}
-
 						{showTitle && title ? (
 							isValidElement(title) && title
 						) : (
