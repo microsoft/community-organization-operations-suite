9d9ccad16e53eb3c212398792d6649d212a092dd
Jonathan Marcello <jonmarcello@gmail.com>
2022-04-11 18:43:56 -0400

    clean up double ternary operator

--- packages/webapp/src/components/lists/ReportList/ReportOptions.tsx
+++ packages/webapp/src/components/lists/ReportList/ReportOptions.tsx
@@ -241,11 +241,7 @@ export const ReportOptions: FC<ReportOptionsProps> = memo(function ReportOptions
 
 	const defaultReportType = reportOptions.find((r) => r.value === type) ?? reportOptions[0]
 
-	const firstService = filterOptions
-		? filterOptions?.options
-			? filterOptions?.options[0]
-			: null
-		: null
+	const firstService = filterOptions ? filterOptions?.options?.[0] : null
 	const firstServiceOption: OptionType = firstService
 		? { value: firstService.value, label: firstService.label }
 		: null
