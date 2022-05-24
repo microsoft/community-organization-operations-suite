66911de41e9ce5cb9d56460b1820aaa2bd31e443
Yohann Paris <github@yohannparis.com>
2022-05-24 11:43:58 -0400

    Start test optimistic responses

--- packages/webapp/src/hooks/api/useEngagementList/addEngagementCallback.ts
+++ packages/webapp/src/hooks/api/useEngagementList/addEngagementCallback.ts
@@ -11,6 +11,7 @@ import type {
 import { EngagementFields } from '../fragments'
 import { useCallback } from 'react'
 import { Namespace, useTranslation } from '~hooks/useTranslation'
+import { GET_ENGAGEMENTS } from '../useEngagementList/useEngagementListData'
 
 const CREATE_ENGAGEMENT = gql`
 	${EngagementFields}
@@ -34,7 +35,50 @@ export function useAddEngagementCallback(orgId: string): AddEngagementCallback {
 
 	return useCallback(
 		(engagementInput: EngagementInput) => {
-			createEngagement({ variables: { engagement: { ...engagementInput, orgId } } })
+			//console.log(engagementInput)
+			createEngagement({
+				variables: { engagement: { ...engagementInput, orgId } },
+				optimisticResponse: {
+					createEngagement: {
+						message: 'Success',
+						engagement: {
+							id: crypto.randomUUID(),
+							orgId: orgId,
+							title: engagementInput.title,
+							description: engagementInput.description,
+							status: 'OPEN', // Keep the default for creation
+							startDate: Date.now(), // TODO: This should be set by the front-end, not the back-end...
+							endDate: engagementInput.endDate ?? null,
+							user: null,
+							tags: engagementInput.tags ?? [],
+							contacts: [],
+							actions: [],
+							__typename: 'Engagement'
+						},
+						__typename: 'EngagementResponse'
+					}
+				},
+				// data is where we can access the optimisticResponse we passed in earlier
+				update: (cache, { data }) => {
+					// Get the current cached data.
+					const engagements = cache.readQuery({
+						// The cached query key is the same as the name of the GQL schema
+						query: GET_ENGAGEMENTS
+					}) as any
+
+					// Now we combine the optimisticResponse we passed in earlier and the existing data
+					const newEngagements = [
+						data.createEngagement.engagement,
+						...engagements.activeEngagements
+					]
+					//console.log({engagements, newEngagements})
+					// Finally we overwrite the cache
+					// cache.writeQuery({
+					//   query: GET_ENGAGEMENTS,
+					//   data: { getPosts: newEngagements },
+					// })
+				}
+			})
 				.then(() => success(c('hooks.useEngagementList.addEngagement.success')))
 				.catch((error) => failure(c('hooks.useEngagementList.addEngagement.failed'), error))
 		},
