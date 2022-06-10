0a9443004ef6d686d348d79e90cf5cb12d55e723
Mark Deitner <mdeitner@uncharted.software>
2022-06-10 15:04:33 -0400

    first pass at offline client add

--- packages/webapp/src/hooks/api/useContacts/useCreateContactCallback.ts
+++ packages/webapp/src/hooks/api/useContacts/useCreateContactCallback.ts
@@ -17,6 +17,8 @@ import { useToasts } from '~hooks/useToasts'
 import type { MessageResponse } from '../types'
 import { useCallback } from 'react'
 import { handleGraphqlResponseSync } from '~utils/handleGraphqlResponse'
+import { GET_ORGANIZATION } from '../useOrganization'
+import { useCurrentUser } from '../useCurrentUser'
 
 const CREATE_CONTACT = gql`
 	${ContactFields}
@@ -31,30 +33,54 @@ const CREATE_CONTACT = gql`
 	}
 `
 
-export type CreateContactCallback = (contact: ContactInput) => Promise<MessageResponse>
+export type CreateContactCallback = (contact: ContactInput) => MessageResponse
 
 export function useCreateContactCallback(): CreateContactCallback {
 	const toast = useToasts()
+	const { orgId } = useCurrentUser()
 	const [createContactGQL] = useMutation<any, MutationCreateContactArgs>(CREATE_CONTACT)
 	const [organization, setOrganization] = useRecoilState<Organization | null>(organizationState)
 	const [, setAddedContact] = useRecoilState<Contact | null>(addedContactState)
 	return useCallback(
-		async (contact) => {
+		(contact) => {
 			let result: MessageResponse
-			await createContactGQL({
+			const newContactTempId = crypto.randomUUID()
+
+			// TODO: We need to add some properties to out optimistic response. These properties are also populated by the GQL resolvers so now we have 2 spots where
+			// this logic happens. It would be nice if we could DRY this out.
+			createContactGQL({
 				variables: { contact },
-				update(_cache, resp) {
+				optimisticResponse: {
+					createContact: {
+						message: 'Success',
+						contact: {
+							...contact,
+							name: { first: contact.first, middle: contact.middle || null, last: contact.last },
+							status: 'ACTIVE',
+							engagements: [],
+							id: newContactTempId,
+							__typename: 'Contact'
+						},
+						__typename: 'ContactResponse'
+					}
+				},
+				// TODO: I need to refactor this so that the success toast does isn't displayed for opti response when online
+				update(cache, resp, newContactTempId) {
 					result = handleGraphqlResponseSync(resp, {
 						toast,
 						successToast: ({ createContact }: { createContact: ContactResponse }) =>
 							createContact.message,
 						onSuccess: ({ createContact }: { createContact: ContactResponse }) => {
+							// TODO: Seems like this gets called after we update the cache, AND after we get the server response...
 							// In kiosk mode, we haven't set this in the store as it would expose other client's data,
 							// so we should not try to update it either as it'd cause an error.
 							if (organization?.contacts) {
+								const filteredContacts = organization.contacts.filter(
+									(contact) => contact.id !== newContactTempId
+								)
 								setOrganization({
 									...organization,
-									contacts: [...organization.contacts, createContact.contact].sort(byFirstName)
+									contacts: [...filteredContacts, createContact.contact].sort(byFirstName)
 								})
 							}
 							// however, we do need the new contact, especially when in that kiosk mode:
@@ -62,12 +88,32 @@ export function useCreateContactCallback(): CreateContactCallback {
 							return createContact.message
 						}
 					})
+
+					// Update the cache with out optimistic response
+					// optimisticResponse or serverResponse
+					const newContact = resp.data.createContact.contact
+
+					const existingOrgData = cache.readQuery({
+						query: GET_ORGANIZATION,
+						variables: { orgId }
+					}) as any
+
+					cache.writeQuery({
+						query: GET_ORGANIZATION,
+						variables: { orgId },
+						data: {
+							organization: {
+								...existingOrgData.organization,
+								contacts: [...existingOrgData.organization.contacts, newContact].sort(byFirstName)
+							}
+						}
+					})
 				}
 			})
 
 			return result
 		},
-		[createContactGQL, organization, setAddedContact, setOrganization, toast]
+		[createContactGQL, organization, orgId, setAddedContact, setOrganization, toast]
 	)
 }
 
