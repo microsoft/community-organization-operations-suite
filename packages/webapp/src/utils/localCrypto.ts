19d071c59f0b76956462b921e676c273bb0b01b7
Jason Baker <jason.baker@gutherie.ca>
2022-06-15 10:49:31 -0400

    Add crypto support for local storage
    - provide a method to clear all user hash data
    - test password method for offline auth.

--- packages/webapp/src/utils/localCrypto.ts
+++ packages/webapp/src/utils/localCrypto.ts
@@ -3,14 +3,15 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import * as bcrypt from 'bcryptjs'
-import { useState } from 'react'
+import * as CryptoJS from 'crypto-js'
 
 const APOLLO_KEY = '-apollo-cache-persist'
 const SALT_KEY = '-hash-salt'
 const SALT_ROUNDS = 12
 const HASH_PWD_KEY = '-hash-pwd'
 const CURRENT_USER_KEY = 'current-user'
-
+const VERIFY_TEXT = 'DECRYPT ME'
+const VERIFY_TEXT_KEY = '-verify'
 /**
  * Check if a salt value has been stored for the given user. Each user will need a salt value to generate an encrypted
  * password that will be stored in the session to allow decryption of the apollo persistent cache.
@@ -49,6 +50,25 @@ const setPwdHash = (uid: string, pwd: string): boolean => {
 	}
 	const hashPwd = bcrypt.hashSync(pwd, salt)
 	window.localStorage.setItem(uid.concat(HASH_PWD_KEY), hashPwd)
+
+	const edata = CryptoJS.AES.encrypt(VERIFY_TEXT, getPwdHash(uid)).toString()
+	window.localStorage.setItem(uid.concat(VERIFY_TEXT_KEY), edata)
+	return true
+}
+
+const testPassword = (uid: string, passwd: string) => {
+	const currentPwdHash = getPwdHash(uid)
+	const edata = window.localStorage.getItem(uid.concat(VERIFY_TEXT_KEY))
+	if (!currentPwdHash || !edata) {
+		return false
+	}
+
+	const dataBytes = CryptoJS.AES.decrypt(edata, currentPwdHash)
+	const data = dataBytes.toString(CryptoJS.enc.Utf8)
+
+	if (data != VERIFY_TEXT) {
+		return false
+	}
 	return true
 }
 
@@ -64,6 +84,12 @@ const setCurrentUser = (uid: string) => {
 	window.localStorage.setItem(CURRENT_USER_KEY, uid)
 }
 
+const clearUser = (uid: string): void => {
+	window.localStorage.removeItem(uid.concat(VERIFY_TEXT_KEY))
+	window.localStorage.removeItem(uid.concat(HASH_PWD_KEY))
+	window.localStorage.removeItem(uid.concat(SALT_KEY))
+}
+
 export {
 	setCurrentUser,
 	getCurrentUser,
@@ -72,5 +98,7 @@ export {
 	getSalt,
 	setPwdHash,
 	getPwdHash,
+	testPassword,
+	clearUser,
 	APOLLO_KEY
 }
