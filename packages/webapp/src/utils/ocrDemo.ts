424ebd901654630389119f2839297e75c5ee0fb2
Adrian <95376249+asylves1@users.noreply.github.com>
2022-07-08 16:32:39 -0400

    remove then from await

--- packages/webapp/src/utils/ocrDemo.ts
+++ packages/webapp/src/utils/ocrDemo.ts
@@ -11,27 +11,21 @@ import { imageOcrData } from './demoData'
 
 const callOCR = async (file) => {
 	const formdata = new FormData()
+	let ocrResult = null
 	formdata.append('file', file)
 
 	const requestOptions = {
 		method: 'POST',
 		body: formdata
 	}
-
-	let ocrResult = null
-	await fetch('https://cbo-ops-suite.azurewebsites.net/api/ocr', requestOptions)
-		.then((response) => response.text())
-		.then((result) => (ocrResult = result))
+	ocrResult = await fetch('https://cbo-ops-suite.azurewebsites.net/api/ocr', requestOptions)
 	return ocrResult
 }
 
 const localFile = async (file) => {
 	const request = await callOCR(file)
-
-	const { keyValuePairs } = request
-	// console.log("Key-Value Pairs:");
 	const result = []
-	for (const { key, value, confidence } of keyValuePairs) {
+	for (const { key, value, confidence } of request) {
 		// console.log("- Key  :", `"${key.content}"`);
 		// console.log("  Value:", `"${value?.content ?? "<undefined>"}" (${confidence})`);
 		result.push({
