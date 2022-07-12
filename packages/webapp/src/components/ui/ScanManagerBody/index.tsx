db9e20f496af9ea7ab2c852e86fe3a0d544a6de0
Yohann Paris <github@yohannparis.com>
2022-07-12 14:39:24 -0400

    OCR Demo code (#649)
    * Ocr Demo code
    
    * Demo Code to connect to Azure function
    
    * remove unneed dependencies
    
    * remove unused style file
    
    * remove then from await
    
    * OCR Demo - working ver of demo
    
    * OCR Demo - fixed linting error
    
    * Ocr Demo - use fluent ui
    
    * Ocr Demo - Add Spinner
    
    * OCR Demo - hide img box until img is loaded
    
    Co-authored-by: Adrian <95376249+asylves1@users.noreply.github.com>

--- packages/webapp/src/components/ui/ScanManagerBody/index.tsx
+++ packages/webapp/src/components/ui/ScanManagerBody/index.tsx
@@ -3,14 +3,8 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import type { StandardFC } from '~types/StandardFC'
-import { Namespace, useTranslation } from '~hooks/useTranslation'
 import { memo } from 'react'
-import { DefaultButton } from '@fluentui/react/lib/Button'
-import cx from 'classnames'
-import styles from './index.module.scss'
-import type { IIconProps } from '@fluentui/react'
-import { useNavCallback } from '~hooks/useNavCallback'
-import { ApplicationRoute } from '~types/ApplicationRoute'
+import { ScanOcrDemo } from '~components/ui/ScanOcrDemo'
 
 interface ScanManagerBodyProps {
 	onClose?: () => void
@@ -19,22 +13,6 @@ interface ScanManagerBodyProps {
 
 export const ScanManagerBody: StandardFC<ScanManagerBodyProps> = memo(
 	function ScanFormPanelBody({}) {
-		const onTakePhotoClick = useNavCallback(ApplicationRoute.ScanImage)
-		const { t } = useTranslation(Namespace.Scan)
-		const circleIcon: IIconProps = { iconName: 'CircleShapeSolid' }
-
-		return (
-			<>
-				<div className={cx(styles.scanManagerTopButtonContainer)}>
-					<DefaultButton
-						text={t('scanManager.startScanButton')}
-						iconProps={circleIcon}
-						onClick={() => {
-							onTakePhotoClick()
-						}}
-					/>
-				</div>
-			</>
-		)
+		return <ScanOcrDemo />
 	}
 )
